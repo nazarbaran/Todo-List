d19ee72401a66039cadd620daf7cf745c53a3fb4
NETrix 7 <netrix777@gmail.com>
2019-02-22 00:16:01 +0200

    Updates

---
+++ precache-manifest.061a6c6768f80100e6975b4aad2074c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7f08c383b3c8a2d14d3f14ee68bbd62",
+    "url": "/static/media/add.f7f08c38.svg"
+  },
+  {
+    "revision": "09b6395e8ef16a8b9b06",
+    "url": "/static/css/main.a4f253a6.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c0d942f54eaf748456a9",
+    "url": "/static/js/2.c0d942f5.chunk.js"
+  },
+  {
+    "revision": "3e100566bfd1060ade4a3428c81911ed",
+    "url": "/static/media/robot.3e100566.svg"
+  },
+  {
+    "revision": "09b6395e8ef16a8b9b06",
+    "url": "/static/js/main.09b6395e.chunk.js"
+  },
+  {
+    "revision": "4b93446cf9abb836dbe76e3d0b5e0e65",
+    "url": "/static/media/check.4b93446c.svg"
+  },
+  {
+    "revision": "28c75ad03485cfa5fae75f8803597261",
+    "url": "/static/media/pointerRight.28c75ad0.svg"
+  },
+  {
+    "revision": "6a02d93e6b301f7a6078e2e9bc82ea00",
+    "url": "/static/media/close.6a02d93e.svg"
+  },
+  {
+    "revision": "6421de6fd3be9a4bc361b8cd76f450c1",
+    "url": "/static/media/userBackground.6421de6f.jpg"
+  },
+  {
+    "revision": "9bc1d7fdac47177bfe8e752ace07664b",
+    "url": "/index.html"
+  }
+];
