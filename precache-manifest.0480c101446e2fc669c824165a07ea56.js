9f098f5a87b47838d05c15895b22b0422b6dd325
NETrix 7 <netrix777@gmail.com>
2019-02-22 10:53:14 +0200

    Updates

---
+++ precache-manifest.0480c101446e2fc669c824165a07ea56.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7f08c383b3c8a2d14d3f14ee68bbd62",
+    "url": "/Todo-List/static/media/add.f7f08c38.svg"
+  },
+  {
+    "revision": "3d0e9e4de69ca35d61d1",
+    "url": "/Todo-List/static/css/main.6778ba4c.chunk.css"
+  },
+  {
+    "revision": "f8c54dd8470eca55c083",
+    "url": "/Todo-List/static/js/runtime~main.f8c54dd8.js"
+  },
+  {
+    "revision": "c0d942f54eaf748456a9",
+    "url": "/Todo-List/static/js/2.c0d942f5.chunk.js"
+  },
+  {
+    "revision": "3e100566bfd1060ade4a3428c81911ed",
+    "url": "/Todo-List/static/media/robot.3e100566.svg"
+  },
+  {
+    "revision": "3d0e9e4de69ca35d61d1",
+    "url": "/Todo-List/static/js/main.3d0e9e4d.chunk.js"
+  },
+  {
+    "revision": "4b93446cf9abb836dbe76e3d0b5e0e65",
+    "url": "/Todo-List/static/media/check.4b93446c.svg"
+  },
+  {
+    "revision": "28c75ad03485cfa5fae75f8803597261",
+    "url": "/Todo-List/static/media/pointerRight.28c75ad0.svg"
+  },
+  {
+    "revision": "6a02d93e6b301f7a6078e2e9bc82ea00",
+    "url": "/Todo-List/static/media/close.6a02d93e.svg"
+  },
+  {
+    "revision": "6421de6fd3be9a4bc361b8cd76f450c1",
+    "url": "/Todo-List/static/media/userBackground.6421de6f.jpg"
+  },
+  {
+    "revision": "64a93ef2d6e1e45583d64d7d4b6ba145",
+    "url": "/Todo-List/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nazarbaran/Todo-List.git/precache-manifest.18f0a1c4f31ac1fe6ae57908363cf676.js"
+  "/Todo-List/precache-manifest.0480c101446e2fc669c824165a07ea56.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nazarbaran/Todo-List.git/index.html", {
+workbox.routing.registerNavigationRoute("/Todo-List/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
